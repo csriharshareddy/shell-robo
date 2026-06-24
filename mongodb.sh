54ffe8e660042be1950bfd371b692af16af75ed4
csriharshareddy <chandupatlasriharshareddy@gmail.com>
2026-06-24 14:05:00 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ component=mongod
 source common.sh
 # adding mongodb repo and installing it
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
-dnf install mongo-org -y
+dnf install mongodb-org -y
 #changing mongod.conf to open port to all servers
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 # enable and restart mongod
